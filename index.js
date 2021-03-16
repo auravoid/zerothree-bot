92dce0a5c3d7f9edc961ab4885a171e5ac080cb4
Jason <32175146+void206551@users.noreply.github.com>
2021-03-16 11:14:58 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Twitter = require('twitter');
+const config = require('./config.json')
+const got = require('got');
+
+const client = new Twitter({
+    consumer_key: config.twtAPIKey || process.env.twtAPIKey,
+    consumer_secret: config.twtAPISecret || process.env.twtAPISecret,
+    access_token_key: config.twtUserToken || process.env.twtUserToken,
+    access_token_secret: config.twtUserSecret || process.env.twtUserSecret
+});
+async function postTweet() {
+    try {
+        const response = await got('https://animechan.vercel.app/api/random');
+        let res = JSON.parse(response.body)
+        client.post('statuses/update', {status: `\"${res.quote}\"\n - ${res.character}`},  function(error, tweet, response) {
+            if(error) console.log(error)
+            console.log(tweet.id_str);
+                client.post('statuses/update', {status: `@Z3R0Three This quote is from ${res.anime}`, in_reply_to_status_id: tweet.id_str}, (error, tweet, response) => {
+                    if(error) console.log(error)
+                    console.log(tweet.id_str)
+                })
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+    setTimeout(postTweet, 7200000)
+}
+
+postTweet().then().catch()
